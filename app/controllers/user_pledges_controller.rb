c713b299a6155c2e015e1a755f5d893111143836
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2019-07-07 00:43:20 +0800

    Add user pledges endpoint

--- app/controllers/user_pledges_controller.rb
+++ app/controllers/user_pledges_controller.rb
@@ -1,22 +1,25 @@
 class UserPledgesController < ApplicationController
   def index
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     render json: @user.pledges
   end
 
   def create
-    @user = User.find(params[:id])
-    
+    @user = User.find(params[:user_id])
+    @pledge = Pledge.find(params[:id])
+    @user_pledge = UserPledge.create(user: @user, pledge: @pledge)
     render json: @user.pledges
   end
 
-  def update
-  end
-
+  # GET /user/:user_id/pledges/:pledge_id
   def show
   end
 
+  # GET /user/:user_id/pledges/:pledge_id
   def destroy
+    user_pledge = UserPledge.where(user_id: params[:user_id], pledge_id: params[:pledge_id]).first
+    user_pledge.destroy
+    head :no_content, status: :ok
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  
   resources :pledges
+  resources :users do 
+    resources :pledges, :controller => :user_pledges
+  end
 end
